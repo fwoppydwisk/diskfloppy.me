8cfb858b77d276448a8075b482cf533397bf926b
Frankie B <floppydisk05@aol.com>
2022-07-05 15:38:32 +0100

    Add existing files

---
+++ res/js/themeswap.js
@@ -0,0 +1,26 @@
+function addStyleSheet(name, id) {
+	var path = '/res/css/' + name + '.min.css';
+	var old = document.getElementById(id);
+	if (old && (old.href != path)) {
+		old.href = path;
+	}
+}
+
+var otherTheme = {
+	'dark': 'light',
+	'light': 'dark',
+};
+
+var currentTheme = localStorage.getItem('theme');
+
+if (!otherTheme.hasOwnProperty(currentTheme)) {
+	currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+addStyleSheet(currentTheme, 'theme');
+
+function toggleTheme() {
+	currentTheme = otherTheme[currentTheme] || 'light';
+	localStorage.setItem('theme', currentTheme);
+	addStyleSheet(currentTheme, 'theme');
+}

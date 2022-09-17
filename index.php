bebea756c44ede78002eb22dbab5c5dd8f5d5f17
Frankie B <floppydisk05@aol.com>
2022-09-17 21:41:44 +0000

    Update homepage

--- index.php
+++ index.php
@@ -43,65 +43,54 @@
 <div class="page">
 <?php require('header.php') ?>
 <div id="pagebody">
+	<div id="content">
+		<p>Hi!</p>
+		<p>This is the personal homepage of <a href="./me/">floppydisk</a>. Every now and then, I
+			dabble in software/hardware <a href="./projects/">projects</a>, take <a href="./pics/">pictures</a>, or write
+			<a href="./interblag/">blogposts</a>.
+		</p>
 
-			<div id="content">
-				<p>Hi!</p>
+		<h1>Projects</h1>
+		<p>Various Projects I've worked on</p>
+		<h2>Personal Projects</h2>
+		<p>Projects I prefer to work on solo</p>
+		<div class="project">
+			<span class="name"><a href="http://weather.diskfloppy.me/">weather.diskfloppy.me</a></span>
+			<br>
+			<a href="/projects/weatherdata">A page to display data from my weather station</a>
+		</div>
 
-				<p>This is the personal homepage of <a href="./me/">floppydisk</a>. Every now and then, I
-					dabble in software/hardware <a href="./projects/">projects</a>, take <a href="./pics/">pictures</a>, or write
-					<a href="./interblag/">blogposts</a>.
-				</p>
+		<div class="project">
+			<span class="name"><a href="http://trivia.diskfloppy.me/">trivia.diskfloppy.me</a></span>
+			<br>
+			<a href="/projects/trivia-api">A REST API to get trivia questions</a>
+		</div>
 
-				<h1>Projects</h1>
+		<h2>Group Projects</h2>
+		<p>Projects I do/have done with friends</p>
+		<div class="project">
+			<span class="name"><a href="https://drivers.nickandfloppy.com/">drivers.nickandfloppy.com</a> (<a href="http://drivers.nickandfloppy.com/">HTTP</a>)</span>
+			<br>
+			<a href="/projects/netdrivers/">Driver indexing and storage website</a>
+		</div>
 
-				<!--<p>Description</p>
+		<h1>News</h1>
+		<p>No news... <i>yet!</i></p>
 
-				<h2>Public Transport Websites</h2>-->
-
-				<div class="project">
-					<span class="name"><a href="https://drivers.nickandfloppy.com/">drivers.nickandfloppy.com</a> (<a href="http://drivers.nickandfloppy.com/">HTTP</a>)</span>
-					<br />
-					<a href="/projects/netdrivers/">Driver indexing and storage website</a>
-				</div>
-
-				<div class="project">
-					<span class="name"><a href="https://software.nickandfloppy.com/">software.nickandfloppy.com</a> (<a href="http://software.nickandfloppy.com/">HTTP</a>)</span>
-					<br />
-					<a href="/projects/softwarerewind/">Basically NetDrivers but with software</a>
-				</div>
-
-				<div class="project">
-					<span class="name"><a href="http://weather.diskfloppy.me/">weather.diskfloppy.me</a></span>
-					<br />
-					<a href="/projects/weatherdata">A page to display data from my weather station</a>
-				</div>
-
-				<h1>News</h1>
-
-				<p>No news... <i>yet!</i></p>
-
-				<h1>Contact</h1>
-
-				<p><strong>Mail</strong>: floppydisk05@aol.com<br />
-					<strong>IRC</strong>: floppydisk @ r-type<br />
-				</p>
-
-				<h1>Elsewhere</h1>
-
-				<p><strong>Twitter</strong>: <a href="https://twitter.com/floppydisk__">@floppydisk__</a><br />
-					<strong>GitHub</strong>: <a href="https://github.com/floppydisk05">floppydisk05</a><br />
-				</p>
-
-			</div> <!-- content -->
-
-			<div id="footer" class="pagefooter">
-				<!-- Created <span class="date">Sat 26 Mar 2011 11:39:56 AM CET</span> -->
-			</div> <!-- footer -->
-
-		</div> <!-- pagebody -->
-
-	</div> <!-- page -->
-
+		<h1>Contact</h1>
+		<p><strong>Mail</strong>: floppydisk05@aol.com<br>
+			<strong>IRC</strong>: floppydisk @ r-type<br>
+		</p>
+		
+		<h1>Elsewhere</h1>
+		<p><strong>Twitter</strong>: <a href="https://twitter.com/floppydisk__">@floppydisk__</a><br>
+			<strong>GitHub</strong>: <a href="https://github.com/floppydisk05">floppydisk05</a><br>
+		</p>
+	</div> <!-- content -->
+	<div id="footer" class="pagefooter">
+		<!-- Created <span class="date">Sat 26 Mar 2011 11:39:56 AM CET</span> -->
+	</div> <!-- footer -->
+</div> <!-- pagebody -->
+</div> <!-- page -->
 </body>
-
 </html>

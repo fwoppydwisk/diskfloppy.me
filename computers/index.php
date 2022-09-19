f4f78e898adae6f6c6bab40f8a8fa19d3862ab2d
Frankie B <floppydisk05@aol.com>
2022-09-19 22:38:20 +0000

    Moar computers

--- computers/index.php
+++ computers/index.php
@@ -20,13 +20,13 @@
 	<div id="content">
 		<table class="computers" border="0">
 			<tr>
-				<td class="computer">
+			<td class="computer">
 					<h1>Lenovo ThinkPad T430 (2012)</h1>
 					<h2>Specifications</h2>
 					<table class="skami">
 						<tr>
 							<td><b>OS</b></td>
-							<td>Arch Linux x86_64 / Windows 10 Pro</td>
+							<td>Arch Linux x86_64 / Windows 7 Pro</td>
 						</tr>
 						<tr>
 							<td><b>LCD Resolution</b></td>
@@ -54,6 +54,38 @@
 						</tr>
 					</table>
 				</td>
+				<td class="computer">
+					<h1>Macbook Pro (2018)</h1>
+					<h2>Specifications</h2>
+					<table class="skami">
+						<tr>
+							<td><b>OS</b></td>
+							<td>macOS 12.5.1</td>
+						</tr>
+						<tr>
+							<td><b>LCD Resolution</b></td>
+							<td>2560x1600 (Retina)</td>
+						</tr>
+						<tr>
+							<td><b>CPU</b></td>
+							<td>Intel i5-8259U (8 cores) @ 2.3GHz</td>
+						</tr>
+						<tr>
+							<td><b>GPU</b></td>
+							<td>Intel Iris Plus Graphics 655</td>
+						</tr>
+						<tr>
+							<td><b>Memory</b></td>
+							<td>8GB</td>
+						</tr>
+						<tr>
+							<td><b>HDD</b></td>
+							<td>Apple SSD AP0256M (250GB)</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
 				<td class="computer">
 					<h1>IBM ThinkPad T40 (2003)</h1>
 					<h2>Specifications</h2>
@@ -84,17 +116,35 @@
 						</tr>
 					</table>
 				</td>
-			</tr>
-			<tr>
 				<td class="computer">
 					<h1>IBM ThinkPad X41 (2005)</h1>
 					<h2>Specifications</h2>
-					<p>TBD</p>
-				</td>
-				<td class="computer">
-					<h1>Dell OptiPlex 745 (USFF, 2007)</h1>
-					<h2>Specifications</h2>
-					<p>TBD</p>
+					<table class="skami">
+						<tr>
+							<td><b>OS</b></td>
+							<td>Windows XP Tablet PC Edition (2005, SP3)</td>
+						</tr>
+						<tr>
+							<td><b>LCD Resolution</b></td>
+							<td>1024x768 (TFT)</td>
+						</tr>
+						<tr>
+							<td><b>CPU</b></td>
+							<td>Intel Pentium M (single core) @ 1.6GHz</td>
+						</tr>
+						<tr>
+							<td><b>GPU</b></td>
+							<td>Mobile Intel 915GM/GMS 910GML Express Chipset Family (128MB)</td>
+						</tr>
+						<tr>
+							<td><b>Memory</b></td>
+							<td>1.5GB (DDR SDRAM)</td>
+						</tr>
+						<tr>
+							<td><b>HDD</b></td>
+							<td>Hitachi HTC426040G9AT00 (40GB)</td>
+						</tr>
+					</table>
 				</td>
 			</tr>
 			<tr>
@@ -111,7 +161,7 @@
 			</tr>
 			<tr>
 				<td class="computer">
-					<h1>Fujitsu Milan (1996)</h1>
+					<h1>Dell OptiPlex 745 (USFF, 2007, x3)</h1>
 					<h2>Specifications</h2>
 					<p>TBD</p>
 				</td>
@@ -125,10 +175,38 @@
 				<td class="computer">
 					<h1>Random Whitebox (???)</h1>
 					<h2>Specifications</h2>
-					<p>TBD</p>
+					<table class="skami">
+						<tr>
+							<td><b>OS</b></td>
+							<td>Windows XP Pro (SP3)</td>
+						</tr>
+						<tr>
+							<td><b>CPU</b></td>
+							<td>AMD Athon-PECM (Thunderbird, 4 cores, 1.2GHz)</td>
+						</tr>
+						<tr>
+							<td><b>GPU</b></td>
+							<td>Hercules 3D Propher 9000 PCI (ATI Radeon 9000, 64MB)</td>
+						</tr>
+						<tr>
+							<td><b>Memory</b></td>
+							<td>500MB</td>
+						</tr>
+						<tr>
+							<td><b>HDD</b></td>
+							<td>Seagate ST3160215ACE (150GB)</td>
+						</tr>
+					</table>
 				</td>
 				<td class="computer">
 					<h1>Mac mini (2014)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
+			</tr>
+			<tr>
+				<td class="computer">
+					<h1>Fujitsu Milan (1996)</h1>
 					<h2>Specifications</h2>
 					<p>TBD</p>
 				</td>

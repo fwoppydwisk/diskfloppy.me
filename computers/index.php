1df501bcc889dce40210d7a305922b9852f9b5f6
Frankie B <floppydisk05@aol.com>
2022-09-19 21:47:34 +0000

    Woah look, c o m p u t e r s

--- computers/index.php
+++ computers/index.php
@@ -6,11 +6,11 @@
 	<?php require('../inc/head.html'); ?>
 
 	<!-- Page-specific -->
-	<title>Calculators</title>
-	<link rel="shortcut icon" href="../res/img/icons/ico/calc.ico" type="image/x-icon">
-	<meta property="og:title" content="Calculators">
-	<meta property="og:description" content="C a l c u l a t o r s">
-	<meta property="og:image" content="/res/img/icons/png/calc.png">
+	<title>Computers</title>
+	<!--<link rel="shortcut icon" href="../res/img/icons/ico/calc.ico" type="image/x-icon">-->
+	<meta property="og:title" content="Computers">
+	<meta property="og:description" content="Computers I own or have owned.">
+	<!--<meta property="og:image" content="/res/img/icons/png/computer.png">-->
 </head>
 <body>
 <div class="page">  
@@ -18,104 +18,122 @@
 
 <div id="pagebody">
 	<div id="content">
-		<h1>Lenovo ThinkPad T430 (2012)</h1>
-		<h2>Specifications</h2>
-		<table class="skami">
+		<table class="computers" border="0">
 			<tr>
-				<td><b>OS</b></td>
-				<td>Arch Linux x86_64 / Windows 10 Pro</td>
+				<td class="computer">
+					<h1>Lenovo ThinkPad T430 (2012)</h1>
+					<h2>Specifications</h2>
+					<table class="skami">
+						<tr>
+							<td><b>OS</b></td>
+							<td>Arch Linux x86_64 / Windows 10 Pro</td>
+						</tr>
+						<tr>
+							<td><b>LCD Resolution</b></td>
+							<td>1600x900</td>
+						</tr>
+						<tr>
+							<td><b>CPU</b></td>
+							<td>Intel i7-3520M (4 cores) @ 3.6GHz</td>
+						</tr>
+						<tr>
+							<td><b>GPU</b></td>
+							<td>Intel 3rd Gen Core processor Graphics Controller</td>
+						</tr>
+						<tr>
+							<td><b>Memory</b></td>
+							<td>16GB</td>
+						</tr>
+						<tr>
+							<td><b>HDD0</b></td>
+							<td>Crucial CT500MX500SSD1 (500GB, Linux)</td>
+						</tr>
+						<tr>
+							<td><b>HDD1</b></td>
+							<td>Crucial CT250MX500SSD1 (250GB, Windows)</td>
+						</tr>
+					</table>
+				</td>
+				<td class="computer">
+					<h1>IBM ThinkPad T40 (2003)</h1>
+					<h2>Specifications</h2>
+					<table class="skami">
+						<tr>
+							<td><b>OS</b></td>
+							<td>Windows XP Pro / Windows 2000 Pro (Extended)</td>
+						</tr>
+						<tr>
+							<td><b>LCD Resolution</b></td>
+							<td>1024x768</td>
+						</tr>
+						<tr>
+							<td><b>CPU</b></td>
+							<td>Intel Pentium M (single core) @ 1.3GHz</td>
+						</tr>
+						<tr>
+							<td><b>GPU</b></td>
+							<td>ATI Mobility Radeon 7500 (32MB)</td>
+						</tr>
+						<tr>
+							<td><b>Memory</b></td>
+							<td>1GB (DDR SDRAM)</td>
+						</tr>
+						<tr>
+							<td><b>HDD</b></td>
+							<td>Fujitsu MHS2030AT (30GB)</td>
+						</tr>
+					</table>
+				</td>
 			</tr>
 			<tr>
-				<td><b>LCD Resolution</b></td>
-				<td>1600x900</td>
+				<td class="computer">
+					<h1>IBM ThinkPad X41 (2005)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
+				<td class="computer">
+					<h1>Dell OptiPlex 745 (USFF, 2007)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
 			</tr>
 			<tr>
-				<td><b>CPU</b></td>
-				<td>Intel i7-3520M (4 cores) @ 3.6GHz</td>
+				<td class="computer">
+					<h1>Dell OptiPlex GX1 (400L+, 1999)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
+				<td class="computer">
+					<h1>Dell Inspiron 1525 (2008)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
 			</tr>
 			<tr>
-				<td><b>GPU</b></td>
-				<td>Intel 3rd Gen Core processor Graphics Controller</td>
+				<td class="computer">
+					<h1>Fujitsu Milan (1996)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
+				<td class="computer">
+					<h1>Compaq Armada M300 (2000)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
 			</tr>
 			<tr>
-				<td><b>Memory</b></td>
-				<td>16GB</td>
-			</tr>
-			<tr>
-				<td><b>HDD0</b></td>
-				<td>Crucial CT500MX500SSD1 (500GB, Linux)</td>
-			</tr>
-			<tr>
-				<td><b>HDD1</b></td>
-				<td>Crucial CT250MX500SSD1 (250GB, Windows)</td>
+				<td class="computer">
+					<h1>Random Whitebox (???)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
+				<td class="computer">
+					<h1>Mac mini (2014)</h1>
+					<h2>Specifications</h2>
+					<p>TBD</p>
+				</td>
 			</tr>
 		</table>
-
-		<h1>IBM ThinkPad T40 (2003)</h1>
-		<h2>Specifications</h2>
-		<table class="skami">
-			<tr>
-				<td><b>OS</b></td>
-				<td>Windows XP Pro / Windows 2000 Pro (Extended)</td>
-			</tr>
-			<tr>
-				<td><b>LCD Resolution</b></td>
-				<td>1024x768</td>
-			</tr>
-			<tr>
-				<td><b>CPU</b></td>
-				<td>Intel Pentium M (single core) @ 1.3GHz</td>
-			</tr>
-			<tr>
-				<td><b>GPU</b></td>
-				<td>ATI Mobility Radeon 7500 (32MB)</td>
-			</tr>
-			<tr>
-				<td><b>Memory</b></td>
-				<td>1GB (DDR SDRAM)</td>
-			</tr>
-			<tr>
-				<td><b>HDD</b></td>
-				<td>Fujitsu MHS2030AT (30GB)</td>
-			</tr>
-		</table>
-
-		<h1>IBM ThinkPad X41 (2005)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Dell OptiPlex 745 (USFF, 2007)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Dell OptiPlex GX1 (400L+, 1999)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Dell Inspiron 1525 (2008)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Fujitsu Milan (1996)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Toshiba Qosimo F20 (2006)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Compaq Armada M300 (2000)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Mac mini 2014 (2014, duh)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
-		<h1>Random Whitebox (???)</h1>
-		<h2>Specifications</h2>
-		<p>TBD</p>
-
 	</div> <!-- content -->
 
 	<div id="footer" class="pagefooter">

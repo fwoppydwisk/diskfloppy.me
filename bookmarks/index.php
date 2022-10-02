d66c9617b678cf9e3d8df30d87bd72d808d780ad
Frankie B <floppydisk05@aol.com>
2022-10-02 17:07:56 +0000

    Bookmarks page

---
+++ bookmarks/index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Global -->
+	<?php require('../inc/head.html'); ?>
+
+	<!-- Page-specific -->
+	<title>Bookmarks</title>
+	<!--<link rel="shortcut icon" href="../res/img/icons/ico/calc.ico" type="image/x-icon">-->
+	<meta property="og:title" content="Bookmarks">
+	<meta property="og:description" content="friends' websites or sites I just find cool">
+	<!--<meta property="og:image" content="/res/img/icons/png/computer.png">-->
+</head>
+<body>
+<div class="page">  
+<?php require('../inc/nav.php') ?>
+
+<div id="pagebody">
+	<div id="content">
+		<h1>Friends' Websites</h1>
+		<ul>
+			<li><a href="http://www.nick99nack.com/">nick99nack</a> - Currently in the process of taking over the internet. I <i>Totally</i> didn't steal any of his stuff.</li>
+			<li><a href="http://dkay.xyz/">dkay</a> - The land of norton.</li>
+			<li><a href="https://campos02.me/">Vbc</a> - Cool brazilian dude, does programming and stuff</li>
+			<li><a href="https://kickfacehq.com/">KickfaceHQ</a> - The interwebz home of Daniel Kickface and Team Kickface! Uses a totally rad FrontPage 2003 theme.</li>
+			<li><a href="https://joshuaalto.com/">Sashi</a> - Site redesign #8! I'll find a website style I enjoy eventually, I swear!</li>
+			<li><a href="http://strangenessnetworks.com/">noone</a> - Strangeness Network, noone's website.</li>
+			<li><a href="https://notashelf.dev/">raf</a> - is a shelf</li>
+			<li><a href="https://starman0620.neocities.org/">CamK06</a> - Now with more outdated HTML!</li>
+			<li><a href="https://hiden.pw/">HIDEN</a> - Moar buttons!</li>
+			<li><a href="http://cocomark.neocities.org/">Coco</a> - needs to go to the brain store</li>
+			<li><a href="http://toxi.pw/">Toxidations</a> - h (idk if this is his actual domain he has like 5)</li>
+			<li><a href="http://xproot.pw/">xproot</a> - a random internet person on this very random planet</li>
+		</ul>
+		<h1>Computing / Technology</h1>
+		<ul>
+			<li><a href="http://www.winworldpc.com/">WinWorld</a> - WinWorld is an online museum dedicated to the preservation and sharing of vintage, abandoned, and pre-release software.</li>
+			<li><a href="http://nina.chat/">NINA</a> - Upcoming revival of the America Online service.</li>
+			<li><a href="http://escargot.chat/">Escargot</a> - MSN & Yahoo Messenger Revival</li>
+		</ul>
+		<h1>Other Cool Stuff</h1>
+		<ul>
+			<li><a href="http://toastytech.com/">ToastyTech</a> - Nathan's Toasty Technology Page</li>
+			<li><a href="http://www.hoary.org/browse/">Optimized for no one</a> - Optimized for no one, but pretty much OK with . . .</li>
+			<li><a href="http://www.cameronsworld.net/">Cameron's World</a> - A love letter to the Internet of old.</li>
+		</ul>
+	</div> <!-- content -->
+
+	<div id="footer" class="pagefooter">
+		<!-- Created <span class="date">Sat 26 Mar 2011 03:11:41 PM CET</span> -->
+	</div> <!-- footer -->
+</div> <!-- pagebody -->
+</div> <!-- page -->
+</body>
+</html>

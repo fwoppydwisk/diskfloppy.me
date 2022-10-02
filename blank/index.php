b5bf3fba8bca900d57b9f31b3f15e0fa574e252e
Frankie B <floppydisk05@aol.com>
2022-10-02 17:07:45 +0000

    Add blank template page

---
+++ blank/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Global -->
+	<?php require('../inc/head.html'); ?>
+
+	<!-- Page-specific -->
+	<title>Bookmarks</title>
+	<!--<link rel="shortcut icon" href="../res/img/icons/ico/calc.ico" type="image/x-icon">-->
+	<meta property="og:title" content="title">
+	<meta property="og:description" content="description">
+	<!--<meta property="og:image" content="/res/img/icons/png/computer.png">-->
+</head>
+<body>
+<div class="page">  
+<?php require('../inc/nav.php') ?>
+
+<div id="pagebody">
+	<div id="content">
+
+	</div> <!-- content -->
+
+	<div id="footer" class="pagefooter">
+		<!-- Created <span class="date">Sat 26 Mar 2011 03:11:41 PM CET</span> -->
+	</div> <!-- footer -->
+</div> <!-- pagebody -->
+</div> <!-- page -->
+</body>
+</html>

773eb29e07c2bde99998172254af9f8117d6663d
Frankie B <floppydisk05@aol.com>
2022-10-26 22:34:12 +0000

    Music stuff

--- index.php
+++ index.php
@@ -17,14 +17,25 @@
 <?php require('./inc/nav.php') ?>
 <div id="pagebody">
 	<div id="content">
-		<br><table class="banner"><tr><td>h</td></tr></table>
+		<!--<br><table class="banner"><tr><td class="banner"><b>Service Message:</b> This is more related to <a class="bannerlink" href="//www.nickandfloppy.com/">nickandfloppy.com</a> but our Gitea service is currently in the process of being migrated to a new VPS. Due to the lengthly migration process, <a class="bannerlink" href="//git.nickandfloppy.com/">git.nickandfloppy.com</a> may periodically be unavailable, though this isn't expected until the final transition.<br><br>~ nick and floppy</td></tr></table>-->
 		<p>Hi!</p>
 		<p>This is the personal homepage of <a href="./me/">floppydisk</a>. Every now and then, I
 			dabble in software/hardware <a href="./projects/">projects</a>, take <a href="./pics/">pictures</a>, or write
 			<a href="./interblag/">blogposts</a>.
 		</p>
 
-		<h1>Projects</h1>
+		<h1>Music <small>(that I like)</small></h1>
+		<table class="skami">
+			<tr>
+				<td><img src="https://geo-media.beatsource.com/image_size/64x64/2/1/9/21919d1e-4c70-4525-864e-73be29a56f2c.jpg"></td>
+				<td width="500"><pre class="music"><b>Title:</b>  Enjoy the Silence
+<b>Album:</b>  The Singles 86>98
+<b>Artist:</b> Depeche Mode
+				</pre></td>
+			</tr>
+		</table>
+
+		<!--<h1>Projects <small>(Individual project pages TBD)</small></h1>
 		<p>Various Projects I've worked on</p>
 		<h2>Personal Projects</h2>
 		<p>Projects I prefer to work on solo</p>
@@ -49,7 +60,7 @@
 		</div>
 
 		<h1>News</h1>
-		<p>No news... <i>yet!</i></p>
+		<p>No news... <i>yet!</i></p>-->
 
 		<h1>Contact</h1>
 		<p><strong>Mail</strong>: floppydisk05@aol.com<br>

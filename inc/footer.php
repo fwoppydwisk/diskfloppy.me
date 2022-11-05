ff217b8540cab3726a68061e955aa83db4b14cde
Frankie B <floppydisk05@aol.com>
2022-11-05 20:58:46 +0000

    Add more buttons to footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <hr>
         <div style="text-align: center;">
-            <a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"></a>&nbsp;&nbsp;<a href=""><img style="border:0;width:88px;height:31px" src="https://www.w3.org/Icons/valid-html401-blue" alt="Valid HTML 4.01!"></a><br>
+            <img src="/res/img/buttons/browser.gif">&nbsp;<a href="https://www.linux.org/"><img src="/res/img/buttons/linuxnow.gif"></a>&nbsp;<a href="https://www.vim.org/"><img src="/res/img/buttons/vim.gif"></a>&nbsp;<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="/res/img/buttons/vcss-blue" alt="Valid CSS!"></a>&nbsp;<a href=""><img style="border:0;width:88px;height:31px" src="/res/img/buttons/valid-html401-blue.png" alt="Valid HTML 4.01!"></a><br>
             Last Modified <span class="date"><?=date('Y-m-d H:i:s', filemtime($file) + 3600);?> GMT</span>
         </div>

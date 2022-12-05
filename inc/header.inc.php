2485f3ff0e06ac7a8f48486811251e3d7f478ee2
Frankie B <floppydisk05@aol.com>
2022-12-05 18:30:58 +0000

    Add theme-color and image alt text

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
     <meta property="og:type" content="website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#333333">
     <link rel="stylesheet" id="theme" href="/res/css/dark.css" type="text/css">
     <script defer data-domain="diskfloppy.me" src="https://analytics.nickandfloppy.com/js/plausible.js"></script>

6a3d6af654bb2605470730627fcf3ac4cb0c5359
Frankie B <floppydisk05@aol.com>
2023-01-26 22:11:46 +0000

    Reformat footer buttons

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,5 +1,9 @@
 <hr>
         <div style="text-align: center;">
-            <img src="/res/img/buttons/browser.gif" alt="Any Damn Browser">&nbsp;<a href="https://www.linux.org/"><img src="/res/img/buttons/linuxnow.gif" alt="Linux NOW!"></a>&nbsp;<a href="https://www.vim.org/"><img src="/res/img/buttons/vim.gif" alt="vim"></a>&nbsp;<a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="/res/img/buttons/vcss-blue.gif" alt="Valid CSS!"></a>&nbsp;<a href=""><img style="border:0;width:88px;height:31px" src="/res/img/buttons/valid-html401-blue.png" alt="Valid HTML 4.01!"></a><br>
+            <img src="/res/img/buttons/browser.gif" alt="Any Damn Browser">&nbsp;
+            <a href="https://www.linux.org/"><img src="/res/img/buttons/linuxnow.gif" alt="Linux NOW!"></a>&nbsp;
+            <a href="https://www.vim.org/"><img src="/res/img/buttons/vim.gif" alt="vim"></a>&nbsp;
+            <a href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="/res/img/buttons/vcss-blue.gif" alt="Valid CSS!"></a>&nbsp;
+            <a href=""><img style="border:0;width:88px;height:31px" src="/res/img/buttons/valid-html401-blue.png" alt="Valid HTML 4.01!"></a><br>
             Last Modified <span class="date"><?=date('Y-m-d H:i:s', filemtime($file) + 3600);?> GMT</span>
         </div>

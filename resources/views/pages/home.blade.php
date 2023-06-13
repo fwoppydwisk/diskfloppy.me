9760968a3f4d7a048b5fc3a7078e92fc1e99650b
Frankie B <git@diskfloppy.me>
2023-06-13 23:07:20 +0100

    Add global layout and homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.default')
+@section('title', 'test')
+@section('description', 'This is the personal homepage of floppydisk.')
+@section('content')
+<p>Hi!</p>
+        <p>This is the personal homepage of floppydisk.</p>
+
+        <!-- Last.fm -->
+        @include('components.lastfm')
+
+        <!-- WeatherData -->
+        @include('components.weatherdata')
+
+        <h1>Contact</h1>
+        <p><strong>Mail</strong>: <a href="mailto:contact@diskfloppy.me">contact@diskfloppy.me</a><br/>
+            <strong>IRC</strong>: <a href="https://r-type.ca/page.php?id=1034">floppydisk @ r-type</a><br/>
+        </p>
+
+        <h1>Elsewhere</h1>
+        <p><strong>Mastodon:</strong> <a rel="me" href="https://mastodon.sdf.org/@floppydisk_">@floppydisk_@mastodon.sdf.org</a><br/>
+            <strong>Twitter:</strong> <a href="https://twitter.com/floppydisk__">@floppydisk__</a><br/>
+            <strong>GitHub:</strong> <a href="https://github.com/floppydisk05">floppydisk05</a><br/>
+            <strong>SDF:</strong> <a href="http://floppydisk.sdf.org/">~floppydisk</a>
+        </p>
+@stop

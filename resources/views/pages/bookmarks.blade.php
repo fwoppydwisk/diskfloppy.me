16ecc6abe33e824c8d101d707890af0e7ccf3101
Frankie B <git@diskfloppy.me>
2023-06-13 23:50:07 +0100

    Add bookmarks page

---
+++ resources/views/pages/bookmarks.blade.php
@@ -0,0 +1,14 @@
+<?php $categories = app('config')->get('bookmarks'); ?>
+@extends('layouts.default')
+@section('title', 'bookmarks')
+@section('description', 'This is the personal homepage of floppydisk.')
+@section('content')
+@foreach ($categories as $category)
+    <h1>{{ $category->name }}</h1>
+    <ul>
+    @foreach ($category->bookmarks as $bookmark)
+        <li><a href="{{ $bookmark->url }}">{{ $bookmark->name }}</a> - {{ $bookmark->description }}</li>
+    @endforeach
+    </ul>
+@endforeach
+@stop

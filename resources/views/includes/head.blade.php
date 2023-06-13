168400fab0ed844fb9476baa99750b41a8d0260c
Frankie B <git@diskfloppy.me>
2023-06-13 23:07:20 +0100

    Add global layout and homepage

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,13 @@
+    <!-- Global -->
+    <meta charset="utf-8">
+    <meta property="og:type" content="website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#333333">
+    <link rel="stylesheet" href="{{ URL::asset ('css/dark.css') }}"/>
+
+    <!-- Page-specific -->
+    <title>~floppydisk / @yield('title')</title>
+    <meta property="og:title" content="~floppydisk / @yield('title')">
+    <meta property="og:description" content="@yield('description')">
+    <meta property="og:image" content="/favicon.png">
+    <style>td{padding:0px;}</style>

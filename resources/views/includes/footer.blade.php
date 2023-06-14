f0ed9a670f2e9b7cdd574af9228702ff05f889d6
Frankie B <git@diskfloppy.me>
2023-06-15 00:06:41 +0100

    why is there even a link here

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,5 +4,5 @@
             <a href="https://www.linux.org/"><img src="{{ URL::asset ('images/buttons/linuxnow.gif') }}" width="88" height="31" alt="Linux NOW!"></a>&nbsp;
             <a href="https://www.vim.org/"><img src="{{ URL::asset ('images/buttons/vim.gif') }}" width="88" height="31" alt="vim"></a>&nbsp;
             <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="{{ URL::asset ('images/buttons/vcss-blue.gif') }}" width="88" height="31" alt="Valid CSS!"></a>&nbsp;
-            <a href=""><img src="{{ URL::asset ('images/buttons/valid-html401-blue.png') }}" width="88" height="31" alt="Valid HTML 4.01!"></a><br/>
+            <img src="{{ URL::asset ('images/buttons/valid-html401-blue.png') }}" width="88" height="31" alt="Valid HTML 4.01!"><br/>
         </div>

c7a1b41a19c9091c9d09a70d7003eaa28813eee1
Frankie B <git@diskfloppy.me>
2023-06-14 23:35:47 +0100

    fix: make configs serializable

--- config/projects.php
+++ config/projects.php
@@ -1,60 +1,71 @@
 <?php
-use App\Models\Project;
-use App\Models\ProjectCategory;
-
 return [
-    new ProjectCategory("Websites", [
-        new Project(
-            "diskfloppy.me",
-            "The website you're looking at right now!",
-            "https://github.com/floppydisk05/diskfloppy.me",
-            [ "PHP", "CSS" ]),
-        new Project(
-            "NetDrivers",
-            "Driver downloads website.",
-            "https://github.com/floppydisk05/NetDrivers",
-            [ "Ruby", "CSS" ])
-    ]),
-    new ProjectCategory("APIs", [
-        new Project(
-            "trivia-api",
-            "API to serve random trivia questions.",
-            "https://github.com/floppydisk05/trivia-api",
-            [ "JavaScript" ]
-        )
-    ]),
-    new ProjectCategory("Discord Bots", [
-        new Project(
-            "PlexBot",
-            "A basic bot to play music from the configured Plex server in a Discord voice channel.",
-            "https://github.com/floppydisk05/PlexBot",
-            [ "Python" ]
-        )
-    ]),
-    new ProjectCategory("Abandoned Projects", [
-        new Project(
-            "website-cf",
-            "Rewrite of my personal website in Adobe ColdFusion.",
-            "https://github.com/floppydisk05/website-cf",
-            [ "Adobe ColdFusion" ]
-        ),
-        new Project(
-            "WinBotJDA",
-            "Rewrite of CamK06's WinBot using Java and DiscordJDA.",
-            "https://github.com/floppydisk05/WinBotJDA",
-            [ "Java" ]
-        ),
-        new Project(
-            "delayed-eject",
-            "Scripts which eject the cd drive a lot to annoy nick.",
-            "https://github.com/floppydisk05/delayed-eject",
-            [ "Shell", "C" ]
-        ),
-        new Project(
-            "php-sound",
-            "Plays a specified sound file or files on the web server when a php page is loaded.",
-            "https://github.com/floppydisk05/php-sound",
-            [ "PHP", "Shell" ]
-        )
-    ])
+    [
+        'name' => "Websites",
+        'projects' => [
+            [
+                'name' => "diskfloppy.me",
+                'description' => "The website you're looking at right now!",
+                'url' => "https://github.com/floppydisk05/diskfloppy.me",
+                'languages' => ["PHP", "CSS"]
+            ],
+            [
+                'name' => "NetDrivers",
+                'description' => "Driver downloads website.",
+                'url' => "https://github.com/floppydisk05/NetDrivers",
+                'languages' => ["Ruby", "CSS"]
+            ]
+        ]
+    ],
+    [
+        'name' => "APIs",
+        'projects' => [
+            [
+                'name' => "trivia-api",
+                'description' => "API to serve random trivia questions.",
+                'url' => "https://github.com/floppydisk05/trivia-api",
+                'languages' => ["JavaScript"]
+            ]
+        ]
+    ],
+    [
+        'name' => "Discord Bots",
+        'projects' => [
+            [
+                'name' => "PlexBot",
+                'description' => "A basic bot to play music from the configured Plex server in a Discord voice channel.",
+                'url' => "https://github.com/floppydisk05/PlexBot",
+                'languages' => ["Python"]
+            ]
+        ]
+    ],
+    [
+        'name' => "Abandoned Projects",
+        'projects' => [
+            [
+                'name' => "website-cf",
+                'description' => "Rewrite of my personal website in Adobe ColdFusion.",
+                'url' => "https://github.com/floppydisk05/website-cf",
+                'languages' => ["Adobe ColdFusion"]
+            ],
+            [
+                'name' => "WinBotJDA",
+                'description' => "Rewrite of CamK06's WinBot using Java and DiscordJDA.",
+                'url' => "https://github.com/floppydisk05/WinBotJDA",
+                'languages' => ["Java"]
+            ],
+            [
+                'name' => "delayed-eject",
+                'description' => "Scripts which eject the cd drive a lot to annoy nick.",
+                'url' => "https://github.com/floppydisk05/delayed-eject",
+                'languages' => ["Shell", "C"]
+            ],
+            [
+                'name' => "php-sound",
+                'description' => "Plays a specified sound file or files on the web server when a php page is loaded.",
+                'url' => "https://github.com/floppydisk05/php-sound",
+                'languages' => ["PHP", "Shell"]
+            ]
+        ]
+    ]
 ];

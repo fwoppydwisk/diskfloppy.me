20e394e9f27aab0cb522f84f3d636b279ecb863e
Frankie B <git@diskfloppy.me>
2023-06-14 22:52:00 +0100

    Move classes to models folder

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,29 @@
+<?php
+class Bookmark {
+    /**
+     * @var string The name of the bookmark.
+     */
+    public $name;
+
+    /**
+     * @var string The URL of the bookmark.
+     */
+    public $url;
+
+    /**
+     * @var string The description of the bookmark.
+     */
+    public $description;
+
+    /**
+     * Bookmark constructor.
+     * @param string $name The name of the bookmark.
+     * @param string $url The URL of the bookmark.
+     * @param string $description The description of the bookmark.
+     */
+    public function __construct($name, $url, $description) {
+        $this->name = $name;
+        $this->url = $url;
+        $this->description = $description;
+    }
+}

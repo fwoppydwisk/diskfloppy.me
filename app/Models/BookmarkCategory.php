ad7f3fca65298e7ffe3ca689ec16e58e6ecd545e
Frankie B <git@diskfloppy.me>
2023-06-14 22:52:00 +0100

    Move classes to models folder

---
+++ app/Models/BookmarkCategory.php
@@ -0,0 +1,22 @@
+<?php
+class BookmarkCategory {
+    /**
+     * @var string The name of the bookmark category.
+     */
+    public $name;
+
+    /**
+     * @var array An array of Bookmark objects.
+     */
+    public $bookmarks;
+
+    /**
+     * BookmarkCategory constructor.
+     * @param string $name The name of the bookmark category.
+     * @param array $bookmarks An array of Bookmark objects.
+     */
+    public function __construct($name, $bookmarks = array()) {
+        $this->name = $name;
+        $this->bookmarks = $bookmarks;
+    }
+}

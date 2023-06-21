c10d758474056d2211cfa42a39c76eccbe5d135e
Frankie B <floppydisk05@aol.com>
2023-06-21 01:54:14 +0100

    fix: actually align it this time

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,15 +12,15 @@
         @include('components.weather')
 
         <h1>Contact</h1>
-        <p><strong>Mail</strong>: <a href="mailto:contact@diskfloppy.me">contact@diskfloppy.me</a><br/>
-            <strong>IRC</strong>:  <a href="https://r-type.ca/page.php?id=1034">floppydisk @ r-type</a><br/>
+        <p><strong>Mail</strong>:&nbsp;<a href="mailto:contact@diskfloppy.me">contact@diskfloppy.me</a><br/>
+            <strong>IRC</strong>:&nbsp;&nbsp;<a href="https://r-type.ca/page.php?id=1034">floppydisk @ r-type</a><br/>
         </p>
 
         <h1>Elsewhere</h1>
-        <p><strong>Mastodon:</strong> <a rel="me" href="https://mastodon.sdf.org/@floppydisk_">@floppydisk_@mastodon.sdf.org</a><br/>
-            <strong>Matrix:</strong>   <a href="https://matrix.to/#/@floppydisk:arcticfoxes.net">@floppydisk:arcticfoxes.net</a><br/>
-            <strong>Twitter:</strong>  <a href="https://twitter.com/floppydisk__">@floppydisk__</a><br/>
-            <strong>GitHub:</strong>   <a href="https://github.com/floppydisk05">floppydisk05</a><br/>
-            <strong>SDF:</strong>      <a href="http://floppydisk.sdf.org/">~floppydisk</a>
+        <p><strong>Mastodon:</strong>&nbsp;<a rel="me" href="https://mastodon.sdf.org/@floppydisk_">@floppydisk_@mastodon.sdf.org</a><br/>
+            <strong>Matrix:</strong>&nbsp;&nbsp;&nbsp;<a href="https://matrix.to/#/@floppydisk:arcticfoxes.net">@floppydisk:arcticfoxes.net</a><br/>
+            <strong>Twitter:</strong>&nbsp;&nbsp;<a href="https://twitter.com/floppydisk__">@floppydisk__</a><br/>
+            <strong>GitHub:</strong>&nbsp;&nbsp;&nbsp;<a href="https://github.com/floppydisk05">floppydisk05</a><br/>
+            <strong>SDF:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://floppydisk.sdf.org/">~floppydisk</a>
         </p>
 @stop

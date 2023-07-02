45fd1d23c4b22d0b1bbd070d799ad3e6f274a8e5
Frankie B <git@diskfloppy.me>
2023-07-02 02:01:54 +0100

    feat: update lastfm & weather to use internal API

--- resources/views/components/weather.blade.php
+++ resources/views/components/weather.blade.php
@@ -1,20 +1,22 @@
 @php
-    function degreesToCompassDirection($degrees) {
-        $cardinalDirections = [
-            'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
-            'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'
-        ];
-        return $cardinalDirections[round($degrees*16/360)];
-    }
+$api_root = app('config')->get('app')['api_root'];
 
-    $data = json_decode(file_get_contents('http://weather.diskfloppy.me/data/weatherData.json'));
-    $updated = gmdate('H:i Y-m-d', intval(rtrim(file_get_contents('http://weather.diskfloppy.me/data/got.txt'))));
-    $data = $data->data->conditions[0];
+function degreesToCompassDirection($degrees) {
+    $cardinalDirections = [
+        'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
+        'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'
+    ];
+    return $cardinalDirections[round($degrees*16/360)];
+}
+
+$data = json_decode(file_get_contents($api_root.'/weather'));
+$updated = gmdate('H:i Y-m-d', $data->updated);
+$data = $data->data;
 @endphp
 
 <h1>Local Weather <small>(Last Update: {{ $updated }})</small></h1>
-        <b>Wind Speed:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $data->wind_speed_last }} mph<br>
-        <b>Wind Direction:</b>&nbsp;{{ $data->wind_dir_last }}°, {{ degreesToCompassDirection($data->wind_dir_last) }}<br>
-        <b>Temperature:</b>&nbsp;&nbsp;&nbsp;&nbsp;{{ round(($data->temp-32)*(5/9), 1) }}°C<br>
-        <b>Rain Rate:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ round($data->rain_rate_last*0.2, 2) }} mm/hr<br>
-        <b>Humidity:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ round($data->hum) }}%<br>
+        <b>Wind Speed:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $data->wind->speed }} mph<br>
+        <b>Wind Direction:</b>&nbsp;{{ $data->wind->direction->degrees }}°, {{ $data->wind->direction->cardinal }}<br>
+        <b>Temperature:</b>&nbsp;&nbsp;&nbsp;&nbsp;{{ $data->temperature }}°C<br>
+        <b>Rain Rate:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $data->rain_rate }} mm/hr<br>
+        <b>Humidity:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $data->humidity }}%<br>

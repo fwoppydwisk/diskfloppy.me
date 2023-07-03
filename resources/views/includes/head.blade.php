1c3408735446569dfcfdb8bc381ebf7e6385b35d
Frankie B <git@diskfloppy.me>
2023-07-03 18:39:11 +0100

    Update favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,6 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#333333">
     <link rel="stylesheet" href="{{ URL::asset ('css/dark.css') }}"/>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16"/>
 
     <!-- Page-specific -->
     <title>~floppydisk / @yield('title')</title>

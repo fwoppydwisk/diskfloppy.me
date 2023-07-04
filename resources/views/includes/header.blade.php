343207e8e4e65cc2b9fdcd97341f4610dc911660
Frankie B <git@diskfloppy.me>
2023-07-04 01:07:57 +0100

    remove 'repos' link from navbar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,6 @@
             <a href="/">home</a> |
             <a href="//git.diskfloppy.me/">git</a> |
 			<a href="/projects/">projects</a> |
-			<a href="https://github.com/floppydisk05?tab=repositories">repos</a> |
 			<a href="/calculators/">calculators</a> |
 			<a href="/computers/">computers</a> |
 			<a href="/bookmarks/">bookmarks</a>

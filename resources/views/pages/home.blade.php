cbdbd2d0c7649e174884ca8ea0b5a5a53672585f
Frankie B <floppydisk05@aol.com>
2023-07-04 18:46:25 +0100

    Remove unnecessary line break

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,14 +14,14 @@
         <h1>Contact</h1>
         <p><strong>Mail</strong>:&nbsp;<a href="mailto:contact@diskfloppy.me">contact@diskfloppy.me</a><br/>
             <strong>IRC</strong>:&nbsp;&nbsp;<a href="https://r-type.ca/page.php?id=1034">floppydisk @ r-type</a><br/>
-        </p>
+	</p>
 
         <h1>Elsewhere</h1>
         <p><strong>Mastodon:</strong>&nbsp;<a rel="me" href="https://c.im/@floppydisk">@floppydisk@c.im</a><br/>
-            <strong>Matrix:</strong>&nbsp;&nbsp;&nbsp;<a href="https://matrix.to/#/@floppydisk:arcticfoxes.net">@floppydisk:arcticfoxes.net</a><br/>
+            <strong>Matrix:</strong>&nbsp;&nbsp;&nbsp;<a href="https://matrix.to/#/@floppydisk:arcticfoxes.net">floppydisk:arcticfoxes.net</a><br/>
             <strong>Twitter:</strong>&nbsp;&nbsp;<a href="https://twitter.com/floppydisk__">@floppydisk__</a><br/>
             <strong>GitHub:</strong>&nbsp;&nbsp;&nbsp;<a href="https://github.com/floppydisk05">floppydisk05</a><br/>
 	    <strong>SDF:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://floppydisk.sdf.org/">~floppydisk</a><br/>
-            <br/><a href="https://stackoverflow.com/users/15933092/floppydisk"><img src="https://stackoverflow.com/users/flair/15933092.png?theme=dark" width="208" height="58" alt="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers"></a>
+            <a href="https://stackoverflow.com/users/15933092/floppydisk"><img src="https://stackoverflow.com/users/flair/15933092.png?theme=dark" width="208" height="58" alt="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers"></a>
 	</p>
 @stop

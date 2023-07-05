b453b177973875f6a59b15077b1cb9e2e3542a27
Frankie B <git@diskfloppy.me>
2023-07-06 00:09:50 +0100

    Update about on homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,8 +2,16 @@
 @section('title', 'home')
 @section('description', 'This is the personal homepage of floppydisk.')
 @section('content')
-        <p>Hi!</p>
-        <p>This is the personal homepage of floppydisk.</p>
+        <h1>About me</h1>
+        <p>Hi!<br/>
+            This is the personal homepage of floppydisk.
+        </p>
+        <h2>Interests</h2>
+        <p>
+            &#9670; <b>Tech Theatre</b> - Lighting, Stage Management, etc.<br/>
+            &#9670; <b>Programming</b>&nbsp; - HTML, JavaScript, C#, Java, PHP, Ruby<br/>
+            &#9670; <b>Photography</b>&nbsp; - <a href="https://www.flickr.com/photos/floppydisk/">Flickr</a>
+        </p>
 
         <!-- Last.fm -->
         @include('components.lastfm')
@@ -14,14 +22,14 @@
         <h1>Contact</h1>
         <p><strong>Mail</strong>:&nbsp;<a href="mailto:contact@diskfloppy.me">contact@diskfloppy.me</a><br/>
             <strong>IRC</strong>:&nbsp;&nbsp;<a href="https://r-type.ca/page.php?id=1034">floppydisk @ r-type</a><br/>
-	</p>
+        </p>
 
         <h1>Elsewhere</h1>
         <p><strong>Mastodon:</strong>&nbsp;<a rel="me" href="https://c.im/@floppydisk">@floppydisk@c.im</a><br/>
             <strong>Matrix:</strong>&nbsp;&nbsp;&nbsp;<a href="https://matrix.to/#/@floppydisk:arcticfoxes.net">@floppydisk:arcticfoxes.net</a><br/>
             <strong>Twitter:</strong>&nbsp;&nbsp;<a href="https://twitter.com/floppydisk__">@floppydisk__</a><br/>
             <strong>GitHub:</strong>&nbsp;&nbsp;&nbsp;<a href="https://github.com/floppydisk05">floppydisk05</a><br/>
-	    <strong>SDF:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://floppydisk.sdf.org/">~floppydisk</a><br/>
+            <strong>SDF:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://floppydisk.sdf.org/">~floppydisk</a><br/>
             <a href="https://stackoverflow.com/users/15933092/floppydisk"><img src="https://stackoverflow.com/users/flair/15933092.png?theme=dark" width="208" height="58" alt="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for floppydisk at Stack Overflow, Q&amp;A for professional and enthusiast programmers"></a>
-	</p>
+        </p>
 @stop

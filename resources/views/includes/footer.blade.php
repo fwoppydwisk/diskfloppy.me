b60a70394faf5a2f63d7e318fecd4961fcb9109e
Frankie B <git@diskfloppy.me>
2023-07-12 00:37:33 +0100

    Update footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,10 @@
 <hr>
-        <div style="text-align: center;">
+        <div class="footer">
             <img src="{{ URL::asset ('images/buttons/browser.gif') }}" width="88" height="31" alt="Any Damn Browser">&nbsp;
             <a href="https://www.linux.org/"><img src="{{ URL::asset ('images/buttons/linuxnow.gif') }}" width="88" height="31" alt="Linux NOW!"></a>&nbsp;
             <a href="https://www.vim.org/"><img src="{{ URL::asset ('images/buttons/vim.gif') }}" width="88" height="31" alt="vim"></a>&nbsp;
             <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="{{ URL::asset ('images/buttons/vcss-blue.gif') }}" width="88" height="31" alt="Valid CSS!"></a>&nbsp;
             <img src="{{ URL::asset ('images/buttons/valid-html401-blue.png') }}" width="88" height="31" alt="Valid HTML 4.01!"><br/>
+            This site is best viewed at 1024x768 with 16-bit color or better<br/>
+            &copy; floppydisk 2021-{{ date('Y') }}, Site Version {{ config('app.version') }}, <a href="https://github.com/floppydisk05/diskfloppy.me">GitHub</a><br/>
         </div>

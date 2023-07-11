0dde11718aca908300a126c274e476feb548c7a2
Frankie B <git@diskfloppy.me>
2023-07-11 19:24:00 +0100

    Update ALL indentation to spaces, not tabs

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,10 @@
-	<nav>
-		<div>
+    <nav>
+        <div>
             <a href="/">home</a> |
             <a href="//git.diskfloppy.me/">git</a> |
-			<a href="/projects/">projects</a> |
-			<a href="/calculators/">calculators</a> |
-			<a href="/computers/">computers</a> |
-			<a href="/bookmarks/">bookmarks</a>
-		</div>
-	</nav>
+            <a href="/projects/">projects</a> |
+            <a href="/calculators/">calculators</a> |
+            <a href="/computers/">computers</a> |
+            <a href="/bookmarks/">bookmarks</a>
+        </div>
+    </nav>

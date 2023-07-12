74c48d7bb65b20c65b207463b5f8fa420849206f
Frankie B <floppydisk05@aol.com>
2023-07-13 00:06:44 +0100

    Add wiki link to navbar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,8 @@
     <nav>
         <div>
             <a href="/">home</a> |
-            <a href="//git.diskfloppy.me/">cgit</a> |
+	    <a href="//git.diskfloppy.me/">cgit</a> |
+            <a href="//wiki.diskfloppy.me/">wiki</a> |
             <a href="/projects/">projects</a> |
             <a href="/calculators/">calculators</a> |
             <a href="/computers/">computers</a> |

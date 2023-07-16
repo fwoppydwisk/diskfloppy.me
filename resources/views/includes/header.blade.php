2a11cff663b9520643bd0178ddc671705dd06e31
Frankie B <git@diskfloppy.me>
2023-07-16 21:02:51 +0100

    feat: site admin (#8)

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,18 @@
     <nav>
         <div>
             <a href="/">home</a> |
-	    <a href="//git.diskfloppy.me/">cgit</a> |
+	        <a href="//git.diskfloppy.me/">cgit</a> |
             <a href="//wiki.diskfloppy.me/">wiki</a> |
             <a href="/projects/">projects</a> |
             <a href="/calculators/">calculators</a> |
             <a href="/computers/">computers</a> |
             <a href="/bookmarks/">bookmarks</a> |
             <a href="/guestbook/">guestbook</a>
+            @if (auth()->check())
+                | <a href="/admin/">admin</a>
+                | ({{ auth()->user()->name }}) <a href="/logout">logout</a>
+            @else
+                | <a href="/login">login</a>
+            @endif
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,46 @@ Route::post('/guestbook', 'App\Http\Controllers\GuestbookController@guestbookpos
     ->name('guestbookPost')
     ->middleware('rate_limit');
 
+Route::get('/admin', function () {
+    if (!auth()->check()) {
+        return view('errors.no-auth');
+    }
+    return view('pages.admin.index');
+});
+
+Route::get('/admin/guestbook', function () {
+    if (!auth()->check()) {
+        return view('errors.no-auth');
+    }
+    return view('pages.admin.guestbook');
+});
+
+Route::get('/admin/guestbook/delete', function () {
+    if (!auth()->check()) {
+        return view('errors.no-auth');
+    }
+
+    $id = request()->input('id');
+    $entry = DB::table('guestbook_entries')->find($id);
+
+    if ($entry) {
+        // Render a confirmation view
+        return View::make('pages.admin.guestbook-del-confirm', compact('entry'));
+    } else {
+        return view('errors.generic-error')
+            ->with('error', "Entry not found")
+            ->with('description', "The specified entry does not exist!");
+    }
+});
+
+Route::post('/admin/guestbook/delete', function () {
+    if (!auth()->check()) {
+        return view('errors.no-auth');
+    }
+
+    $id = request()->input('id');
+    DB::table('guestbook_entries')->where('id', $id)->delete();
+
+    return back()->with('success', 'Entry deleted successfully!');
+});
 

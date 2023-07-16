7d75d20ac6979f43bc3f6b2be91efe19845eb8b2
Frankie B <git@diskfloppy.me>
2023-07-16 21:01:30 +0100

    feat: site admin pages

---
+++ resources/views/errors/generic-error.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.minimal')
+@section('title', 'Error 401: Unauthorized User!')
+@section('content')
+    <h1>{{ $error }}</h1>
+    <hr/>
+    @if(isset($description))
+        <p>{{ $description }}</p>
+    @endif
+@stop

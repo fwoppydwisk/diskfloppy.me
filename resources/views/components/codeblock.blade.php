eda34f5bfecfd917f6641f483eba4f1174cb01cc
Frankie B <git@diskfloppy.me>
2023-07-28 23:45:33 +0100

    Add hlphp & codeblock component

---
+++ resources/views/components/codeblock.blade.php
@@ -0,0 +1,14 @@
+@php
+$file_path = "code/".$file;
+$file_name = basename($file_path);
+
+$hl = new \Highlight\Highlighter();
+
+$highlighted = $hl->highlight($lang, Storage::disk('local')->get($file_path));
+$file_size = Storage::disk('local')->size($file_path);
+@endphp
+<div class="codeblock"><pre>
+    <h1>{{ $file_name }} <small>{{ $file_size}} bytes</small></h1>
+    <hr>
+    <code class="hljs {{$highlighted->language}}">{!! $highlighted->value !!}</code>
+</pre></div>

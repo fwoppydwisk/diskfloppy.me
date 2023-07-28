9a10c8db3ade25b1f9e920d0860063b95bd00dd6
Frankie B <git@diskfloppy.me>
2023-07-28 23:44:46 +0100

    Update header to new design

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,27 +1,18 @@
     <nav>
         <div>
-            <!--<a href="/">home</a> |
- <a href="//git.diskfloppy.me/">cgit</a> |
-            <a href="/projects/">projects</a> |
-            <a href="/calculators/">calculators</a> |
-            <a href="/computers/">computers</a> |
-            <a href="/bookmarks/">bookmarks</a> |
-            <a href="/guestbook/">guestbook</a>
-            @if (auth()->check())
-| <a href="/admin/">admin</a>
-                | ({{ auth()->user()->name }}) <a href="/logout">logout</a>
-@else
-| <a href="/login">login</a>
-@endif
-            -->
             <h1>diskfloppy.me | <strong>@yield('title')</strong></h1>
-            <a href="/" title="Home"><img src="/images/icons/nav/home.png"></a>
-            <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png"></a>
-            <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png"></a>
-            <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png"></a>
-            <a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png"></a>
-            <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png"></a>
-            <a href="/music/" title="Music"><img src="/images/icons/nav/music.png"></a>&nbsp;&nbsp;
-            <a href="/login/" title="Log in"><img src="/images/icons/nav/login.png"></a>
+            <a href="/" title="Home"><img src="/images/icons/nav/home.png" alt="Home"></a>
+            <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png" alt="cgit"></a>
+            <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png" alt="Bookmarks"></a>
+            <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png" alt="Guestbook"></a>
+            <a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png" alt="Contact"></a>
+            <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png" alt="Weather"></a>
+            <a href="/music/" title="Music"><img src="/images/icons/nav/music.png" alt="Music"></a>&nbsp;&nbsp;
+            @if (auth()->check())
+                <a href="/admin/">admin</a>
+                ({{ auth()->user()->name }}) <a href="/logout">logout</a>
+            @else
+                <a href="/login/" title="Login"><img src="/images/icons/nav/login.png" alt="Login"></a>
+            @endif
         </div>
     </nav>

d781702215dd001a2ae19715bac7dd031bbaf6e2
Frankie B <git@diskfloppy.me>
2023-07-29 17:25:04 +0100

    More fixes

--- resources/views/pages/admin/guestbook.blade.php
+++ resources/views/pages/admin/guestbook.blade.php
@@ -10,19 +10,19 @@
     @endphp
     <h1>Entries <small>({{ count($entries) }} total)</small></h1>
     @foreach ($entries as $entry)
-        <table class="gb_admin">
+        <table class="gb-admin">
             <tr>
                 <td>
                     Name:&nbsp;{{ $entry->name }}<br>
                     IP:&nbsp;&nbsp;&nbsp;{{ $entry->ip_address }}<br>
                     Date:&nbsp;{{ gmdate("H:i:s - Y-m-d", $entry->timestamp) }}
                 </td>
-                <td class="gb_del">
+                <td class="gb-del">
                     <a href="/admin/guestbook/delete?id={{ $entry->id }}">del</a>
                 </td>
             </tr>
             <tr>
-                <td colspan="2" class="gb_message">
+                <td colspan="2" class="gb-message">
                     <br>
                     {{ htmlspecialchars($entry->message) }}
                 </td>

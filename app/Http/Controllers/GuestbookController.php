eabff07f08f5b37c4676eb109c588231ad66c72b
Frankie B <git@diskfloppy.me>
2023-07-29 17:15:39 +0100

    Partial fixes

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -18,17 +18,20 @@ class GuestbookController extends Controller {
 
         $matching_bans = DB::select('SELECT reason FROM guestbook__bans WHERE ip_address = ?', array($request->ip()));
 
-        if (count($matching_bans) > 0 ) {
+        if (!empty($matching_bans)) {
             return view('errors.guestbook-ipban')->with('reason', $matching_bans[0]->reason);
         }
 
-        DB::insert('INSERT INTO guestbook__entries (name, timestamp, ip_address, agent, message) values (?, ?, ?, ?, ?)', array(
-            htmlspecialchars($request->get('name')),
-            time(),
-            $request->ip(),
-            $request->userAgent(),
-            htmlspecialchars($request->get('message'))
-        ));
+        DB::insert(
+            'INSERT INTO guestbook__entries (name, timestamp, ip_address, agent, message) values (?, ?, ?, ?, ?)',
+            array(
+                htmlspecialchars($request->get('name')),
+                time(),
+                $request->ip(),
+                $request->userAgent(),
+                htmlspecialchars($request->get('message'))
+            )
+        );
 
         return back()->with('success', 'Entry submitted successfully!');
     }

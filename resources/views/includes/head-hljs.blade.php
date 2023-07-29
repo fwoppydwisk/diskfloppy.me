e558997a6a46263af1b8286910fe8df876300687
Frankie B <git@diskfloppy.me>
2023-07-29 18:14:12 +0100

    Remove unused highlighting styles, change "highlight.js" to just "highlight"

--- resources/views/includes/head-hljs.blade.php
+++ resources/views/includes/head-hljs.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#333333">
     <link rel="stylesheet" href="{{ URL::asset ('css/master.css') }}"/>
-    <link rel="stylesheet" href="{{ URL::asset ('css/highlight.js/tomorrow-night-bright.css') }}"/>
+    <link rel="stylesheet" href="{{ URL::asset ('css/highlight/tomorrow-night-bright.css') }}"/>
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
 

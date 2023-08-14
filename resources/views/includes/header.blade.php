9d22db13a53fd92efd5102acd3a923cf16724225
Frankie B <git@diskfloppy.me>
2023-08-15 00:04:27 +0100

    Add width/height to nav images

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,13 +1,13 @@
     <nav>
         <div>
             <h1>diskfloppy.me | <strong>@yield('title')</strong></h1>
-            <a href="/" title="Home"><img src="/images/icons/nav/home.png" alt="Home"></a>
-            <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png" alt="cgit"></a>
-            <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png" alt="Bookmarks"></a>
-            <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png" alt="Guestbook"></a>
-            <!--<a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png" alt="Contact"></a>-->
-            <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png" alt="Weather"></a>
-            <a href="/music/" title="Music"><img src="/images/icons/nav/music.png" alt="Music"></a>&nbsp;&nbsp;
+            <a href="/" title="Home"><img src="/images/icons/nav/home.png" alt="Home" width="32" height="32"></a>
+            <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png" alt="cgit" width="32" height="32"></a>
+            <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png" alt="Bookmarks" width="32" height="32"></a>
+            <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png" alt="Guestbook" width="32" height="32"></a>
+            <!--<a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png" alt="Contact" width="32" height="32"></a>-->
+            <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png" alt="Weather" width="32" height="32"></a>
+            <a href="/music/" title="Music"><img src="/images/icons/nav/music.png" alt="Music" width="32" height="32"></a>&nbsp;&nbsp;
             @if (auth()->check())
                 <a href="/admin/">admin</a>
                 ({{ auth()->user()->name }}) <a href="/logout">logout</a>

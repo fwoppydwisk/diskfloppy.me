e7d58f3185b798df4e020f0c2b7ab36d1d1656dc
Frankie B <git@diskfloppy.me>
2023-08-14 22:51:16 +0100

    Actually v6

--- resources/views/components/codeblock.blade.php
+++ resources/views/components/codeblock.blade.php
@@ -7,8 +7,8 @@ $hl = new \Highlight\Highlighter();
 $highlighted = $hl->highlight($lang, Storage::disk('local')->get($file_path));
 $file_size = Storage::disk('local')->size($file_path);
 @endphp
-<div class="codeblock"><pre>
+<div class="codeblock">
     <h1>{{ $file_name }} <small>{{ $file_size}} bytes</small></h1>
     <hr>
     <code class="hljs {{$highlighted->language}}">{!! $highlighted->value !!}</code>
-</pre></div>
+</div>

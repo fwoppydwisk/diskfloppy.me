7943c6b6e3788e1410d3fb72b3917801c7cf4dbd
Frankie B <git@diskfloppy.me>
2023-08-16 14:46:18 +0100

    Swap out some buttons

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,16 +1,29 @@
 <hr>
 <div class="footer">
-    <img src="{{ URL::asset('images/buttons/browser.gif') }}" width="88" height="31" alt="Any Damn Browser">&nbsp;
-    <a href="https://dimden.dev/"><img src="https://dimden.dev/services/images/88x31.gif" alt="dimden.dev"></a>&nbsp;
-    <a href="https://www.linux.org/"><img src="{{ URL::asset('images/buttons/linuxnow.gif') }}" width="88"
-            height="31" alt="Linux NOW!"></a>&nbsp;
-    <a href="https://www.vim.org/"><img src="{{ URL::asset('images/buttons/vim.gif') }}" width="88" height="31"
-            alt="vim"></a>&nbsp;
-    <a href="http://jigsaw.w3.org/css-validator/check/referer"><img
+    <a href="https://www.w3schools.com/html/">
+        <img src="{{ URL::asset('images/buttons/html_learn_it_today.gif') }}"
+             width="88" height="31" alt="Learn HTML today!">&nbsp;
+    </a>
+    <a href="https://dimden.dev/">
+        <img src="https://dimden.dev/services/images/88x31.gif" alt="dimden.dev">
+    </a>&nbsp;
+    <a href="https://www.linux.org/">
+        <img src="{{ URL::asset('images/buttons/linuxnow.gif') }}" width="88"
+             height="31" alt="Linux NOW!">
+    </a>&nbsp;
+    <a href="https://www.vim.org/">
+        <img src="{{ URL::asset('images/buttons/vim.gif') }}" width="88" height="31"
+             alt="vim">
+    </a>&nbsp;
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img
             src="{{ URL::asset('images/buttons/vcss-blue.gif') }}" width="88" height="31"
-            alt="Valid CSS!"></a>&nbsp;
-    <img src="{{ URL::asset('images/buttons/valid-html401-blue.png') }}" width="88" height="31"
-        alt="Valid HTML 4.01!"><br>
+            alt="Valid CSS!">
+    </a>&nbsp;
+    <a href="https://wiby.me/">
+        <img src="{{ URL::asset('images/buttons/wiby.gif') }}" width="88" height="31"
+             alt="Wiby - Search Engine for the Classic Web">
+    </a><br>
     This site is best viewed at 1024x768 with 16-bit color or better<br>
     &copy; floppydisk 2021-{{ date('Y') }}, Site Version {{ config('app.version') }}, <a
         href="https://github.com/floppydisk05/diskfloppy.me">Source</a><br>

e144965ed5c63c1a81440c3379bd878a0d0b4125
Frankie B <git@diskfloppy.me>
2023-08-22 12:03:05 +0100

    Add "placeholder" bot page

---
+++ resources/views/pages/bot.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('title', 'Discord Bot')
+@section('description', '')
+@section('content')
+    <p>The diskfloppy.me Discord bot blah blah blah blah blah</p>
+    <p>Maybe I'll finish this page later idk</p>
+@stop

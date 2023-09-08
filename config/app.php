88b5abee0b4169cbc18d44d9b3168b5fece61608
Frankie B <git@diskfloppy.me>
2023-09-08 22:43:19 +0100

    Remove commented out crap

--- config/app.php
+++ config/app.php
@@ -28,6 +28,5 @@ return [
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 ];

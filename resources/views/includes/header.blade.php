98b36dd0890d61b7aaa9bb13112c65f5e354a73a
Frankie B <git@diskfloppy.me>
2023-10-15 12:41:15 +0100

    Update header style, add theme to head

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,18 +1,25 @@
     <nav>
-        <div>
-            <h1>diskfloppy.me | <strong>@yield('title')</strong></h1>
-            <a href="/" title="Home"><img src="/images/icons/nav/home.png" alt="Home" width="32" height="32"></a>
-            <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png" alt="cgit" width="32" height="32"></a>
-            <a href="/pub/" title="Public Files"><img src="/images/icons/nav/pubfiles.png" alt="Public Files" width="32" height="32"></a>
-            <a href="/computers/" title="Computers"><img src="/images/icons/nav/computers.png" alt="Computers" width="32" height="32"></a>
-            <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png" alt="Bookmarks" width="32" height="32"></a>
-            <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png" alt="Guestbook" width="32" height="32"></a>
-            <!--<a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png" alt="Contact" width="32" height="32"></a>-->
-            <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png" alt="Weather" width="32" height="32"></a>
-            <a href="/music/" title="Music"><img src="/images/icons/nav/music.png" alt="Music" width="32" height="32"></a>&nbsp;&nbsp;
-            @if (auth()->check())
-                <a href="/admin/">admin</a>
-                ({{ auth()->user()->name }}) <a href="/logout">logout</a>
-            @endif
+        <h1>diskfloppy.me | <strong>@yield('title')</strong></h1>
+        <div class="nav-wrapper">
+            <div>
+                <a href="/" title="Home"><img src="/images/icons/nav/home.png" alt="Home" width="32" height="32"></a>
+                <a href="//git.diskfloppy.me/" title="cgit"><img src="/images/icons/nav/repo.png" alt="cgit" width="32" height="32"></a>
+                <a href="/pub/" title="Public Files"><img src="/images/icons/nav/pubfiles.png" alt="Public Files" width="32" height="32"></a>
+                <a href="/computers/" title="Computers"><img src="/images/icons/nav/computers.png" alt="Computers" width="32" height="32"></a>
+                <a href="/bookmarks/" title="Bookmarks"><img src="/images/icons/nav/bookmarks.png" alt="Bookmarks" width="32" height="32"></a>
+                <a href="/guestbook/" title="Guestbook"><img src="/images/icons/nav/guestbook.png" alt="Guestbook" width="32" height="32"></a>
+                <!--<a href="/contact/" title="Contact"><img src="/images/icons/nav/mail.png" alt="Contact" width="32" height="32"></a>-->
+                <a href="/weather/" title="Weather"><img src="/images/icons/nav/weather.png" alt="Weather" width="32" height="32"></a>
+                <a href="/music/" title="Music"><img src="/images/icons/nav/music.png" alt="Music" width="32" height="32"></a>&nbsp;&nbsp;
+            </div>
+            <div class="theme-selector">
+                <form>
+                    <label for="theme">Theme</label>
+                    <select name="theme" id="theme-selector" onmousedown="this.value='';" onchange="swapTheme(this.value);">
+                        <option value="catppuccin-macchiato">Catppuccin Macchiato</option>
+                        <option value="blind-yourself">blind yourself</option>
+                    </select>
+                </form>
+            </div>
         </div>
     </nav>

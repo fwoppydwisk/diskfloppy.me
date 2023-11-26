72558b2d5ec44d496bf1cdce61236a8145befde0
Frankie B <git@diskfloppy.me>
2023-11-26 15:38:09 +0000

    Add snow

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
     'name' => env('APP_NAME', 'diskfloppy.me'),
-    'version' => '2023.10.16',
+    'version' => '2023.11.26',
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://localhost'),

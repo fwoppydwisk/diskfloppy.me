725712aa35a989f1c2f179e210b8fa1c310e39e9
Frankie B <git@diskfloppy.me>
2024-01-12 18:57:10 +0000

    Add custom error pages, pixel images, etc (also improve computers)

--- resources/views/pages/computers.blade.php
+++ resources/views/pages/computers.blade.php
@@ -2,7 +2,135 @@
 @section('title', 'Computers')
 @section('description', 'Computers I own or have owned.')
 @section('content')
-    <table class="computer" role="presentation">
+    <table class="computers">
+        <tr>
+            <th>PICTURES</th>
+            <th>SPECS &amp; DESCRIPTION</th>
+        </tr>
+        <tr>
+            <td>2023 MacBook Pro 14"</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Apple M3 Pro</li>
+                    <li>18GB RAM</li>
+                    <li>500GB SSD</li>
+                    <li>macOS Sonoma</li>
+                </ul>
+                <p class="description">WHAT</p>
+            </td>
+        </tr>
+        <tr>
+            <td>2018 MacBook Pro 13"</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel i5-8259U @ 2.3GHz</li>
+                    <li>Intel Iris Plus Graphics 655</li>
+                    <li>8GB RAM</li>
+                    <li>250GB SSD</li>
+                    <li>macOS Sonoma</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2012 Lenovo ThinkPad T430</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Core i7</li>
+                    <li>16GB RAM</li>
+                    <li>Windows 7 Professional</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2005 IBM ThinkPad X41T</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Pentium M @ 1.6GHz</li>
+                    <li>Mobile Intel Express Chipset Family (128MB)</li>
+                    <li>1.5GB RAM</li>
+                    <li>40GB HDD</li>
+                    <li>Windows XP Tablet PC Edition</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>1999 Dell OptiPlex GX1</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Pentium II (Deschutes) @ 400MHz</li>
+                    <li>ATI 3D Rage Pro (4MB)</li>
+                    <li>639MB</li>
+                    <li>40GB HDD</li>
+                    <li>MS-DOS 6.22 &amp; WFW 3.10</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2003 IBM ThinkPad T40</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Pentium M @ 1.3GHz</li>
+                    <li>ATI Mobility Radeon 7500 (32MB)</li>
+                    <li>1GB RAM</li>
+                    <li>30GB HDD</li>
+                    <li>Windows 2000 Professional</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2010 HP Compaq Elite 8100</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Core i7</li>
+                    <li>16GB RAM</li>
+                    <li>some SSD and an HDD</li>
+                    <li>Windows Vista Ultimate (64-bit)</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>2014 Mac mini</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Core i5-4278U @ 2.6GHz</li>
+                    <li>Intel Iris Graphics</li>
+                    <li>8GB RAM</li>
+                    <li>1TB HDD</li>
+                    <li>VMware ESXi 6.7.0u3</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>1996 Fujitsu Milan</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Pentium</li>
+                    <li>32MB RAM</li>
+                    <li>1215MB HDD</li>
+                    <li>Windows 98 SE</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>1999 Compaq Armada M300</td>
+            <td>
+                <section-title>Quick Specs</section-title>
+                <ul>
+                    <li>Intel Pentium III</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <!--<table class="computer" role="presentation">
         <tr>
             <td colspan="2"><h2>Custom Build</h2><hr></td>
         </tr>
@@ -296,7 +424,7 @@
                 </table>
             </td>
         </tr>
-    </table>
+    </table>-->
     <!--<table class="computer" role="presentation">
         <tr>
             <td colspan="2"><h2>Compaq Armada M300</h2><hr></td>

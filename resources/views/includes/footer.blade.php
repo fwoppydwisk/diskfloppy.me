4cded68dc45ba59cb5fbe878494e24549b05bff6
Frankie B <git@diskfloppy.me>
2024-01-22 00:09:48 +0000

    CSS updates and a whole theme selector thing

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -28,4 +28,36 @@
     &copy; floppydisk 2021-{{ date('Y') }}, v{{ config('app.version') }}, <a
         href="https://github.com/floppydisk05/diskfloppy.me">Source</a>,
 	Served by {{ gethostname() }}<br>
+    <label for="scheme-selector">Color Scheme:</label>
+    <select onchange="swapScheme(this.value)" id="scheme-selector">
+        <optgroup label="Misc">
+            <option value="c64">C64</option>
+        </optgroup>
+        <optgroup label="Light">
+            <option value="catppuccin-latte">Catppuccin Latte</option>
+            <option value="gruvbox">Gruvbox</option>
+            <option value="man-page">Man Page</option>
+            <option value="papercolor-light">Papercolor Light</option>
+            <option value="solarized-light">Solarized Light</option>
+            <option value="terminal-basic">Terminal Basic</option>
+        </optgroup>
+        <optgroup label="Dark">
+            <option value="catppuccin-frappe">Catppuccin Frappé</option>
+            <option value="catppuccin-macchiato" selected="selected">Catppuccin Macchiato</option>
+            <option value="catppuccin-mocha">Catppuccin Mocha</option>
+            <option value="gruvbox-dark">Gruvbox Dark</option>
+            <option value="gruvbox-material">Gruvbox Material</option>
+            <option value="maia">Maia</option>
+            <option value="mono-amber">Mono Amber</option>
+            <option value="mono-cyan">Mono Cyan</option>
+            <option value="mono-green">Mono Green</option>
+            <option value="mono-red">Mono Red</option>
+            <option value="mono-white">Mono White</option>
+            <option value="mono-yellow">Mono Yellow</option>
+            <option value="papercolor-dark">Papercolor Dark</option>
+            <option value="shel">Shel</option>
+            <option value="slate">Slate</option>
+            <option value="solarized-dark">Solarized Dark</option>
+        </optgroup>
+    </select><br>
 </div>

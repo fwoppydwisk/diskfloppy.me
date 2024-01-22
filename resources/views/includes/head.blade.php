6f83c7710949ffb5f79302525f15796f6083e52b
Frankie B <git@diskfloppy.me>
2024-01-22 01:16:42 +0000

    Get scheme cookie server-side instead of client-side on page load

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,13 +1,16 @@
+@php // Get colorscheme from cookie and apply immediately
+    $colorscheme = request()->cookie('colorscheme', 'catppuccin-macchiato');
+@endphp
     <!-- Global -->
     <meta charset="utf-8">
     <meta property="og:type" content="website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#333333">
-    <link rel="stylesheet" href="/css/colorschemes/catppuccin-macchiato.css" id="css-colorscheme"/>
+    <link rel="stylesheet" href="{{ asset("/css/colorschemes/{$colorscheme}.css") }}" id="css-colorscheme"/>
     <link rel="stylesheet" href="/css/master.css"/>
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
-    <script src="/js/themeSwap.js"></script>
+    <script src="/js/schemeSwap.js"></script>
     {!! (intval(date('n')) == 12) ? '<script src="/js/christmas/snow.js"></script>' : '' !!}
 
     <!-- Page-specific -->

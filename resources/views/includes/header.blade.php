460e9585d22090d7d9417e1ffd57e72838378005
Frankie B <git@diskfloppy.me>
2024-01-22 00:09:20 +0000

    Update guestbook Update weather link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,7 +9,7 @@
                 <a href="/bookmarks/" title="Bookmarks"><img class="pixel" src="/images/icons/nav/bookmarks.png" alt="Bookmarks" width="32" height="32"></a>
                 <a href="/guestbook/" title="Guestbook"><img class="pixel" src="/images/icons/nav/guestbook.png" alt="Guestbook" width="32" height="32"></a>
                 <!--<a href="/contact/" title="Contact"><img class="pixel" src="/images/icons/nav/mail.png" alt="Contact" width="32" height="32"></a>-->
-                <a href="/weather/" title="Weather"><img class="pixel" src="/images/icons/nav/weather.png" alt="Weather" width="32" height="32"></a>
+                <a href="//weather.diskfloppy.me/" title="Weather"><img class="pixel" src="/images/icons/nav/weather.png" alt="Weather" width="32" height="32"></a>
                 <a href="/music/" title="Music"><img class="pixel" src="/images/icons/nav/music.png" alt="Music" width="32" height="32"></a>&nbsp;&nbsp;
             </div>
         </div>

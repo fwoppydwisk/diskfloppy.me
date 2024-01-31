36c66c75fb6fd72dfb36e4e9b9b8c332737aeb1d
Frankie B <git@diskfloppy.me>
2024-01-31 21:17:56 +0000

    Add database migrations

--- database/migrations/2024_01_31_204815_create_guestbook__bans_table.php
+++ database/migrations/2024_01_31_204815_create_guestbook__bans_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('guestbook__bans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip_address', 40);
+            $table->string('reason', 50);
+            $table->timestamps();
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('guestbook__bans');
     }
 };

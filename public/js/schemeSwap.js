6f695daddceff036cb969eed268e9ada7741dc8a
Frankie B <git@diskfloppy.me>
2024-07-12 22:43:15 +0100

    Use the current hostname in schemeSwap.js instead of hard coding it

--- public/js/schemeSwap.js
+++ public/js/schemeSwap.js
@@ -26,10 +26,11 @@ function getCookie(cname) {
  * @param {number} exdays Cookie lifespan (days)
  */
 function setCookie(cname, cvalue, exdays) {
+    const hostname = window.location.hostname;
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict;Domain=.diskfloppy.me";
+    document.cookie = `${cname}=${cvalue};${expires};path=/;SameSite=Strict;Domain=${hostname}`
 }
 
 /**

5ed8fdc83e3a720c7ef5592a48a18de4b17e623e
Frankie B <git@diskfloppy.me>
2024-07-12 23:48:06 +0100

    Improve computers page, change page width to 975px to compensate for scrollbars

--- resources/views/computers.blade.php
+++ resources/views/computers.blade.php
@@ -2,181 +2,251 @@
     <x-slot:title>Computers</x-slot:title>
     <table class="computers">
         <tr>
-            <th>PICTURES</th>
-            <th>SPECS &amp; DESCRIPTION</th>
+            <th>MODEL</th>
+            <th>CPU</th>
+            <th>GPU</th>
+            <th>STORAGE</th>
+            <th>RAM</th>
+            <th>OS</th>
         </tr>
         <tr>
-            <td>Random Whitebox</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>486DX2 (50MHz)</li>
-                    <li>16MB RAM</li>
-                    <li>280MB HDD</li>
-                    <li>Windows NT 3.51</li>
-                </ul>
-                <p class="description">
-                    Had been monitoring the ventilation system in a school since the late 1990s,
-                    only stopped because the power supply internally exploded. Replaced the PSU with
-                    a standard ATX PSU and an ATX to AT adaptor and it sprung back to life.
-                    Motherboard is a Gigabyte GA486IM with 4 PCI slots, 4 ISA slots and 2 VLB slots.
-                    Has two identical ISA serial/parallel/game-port cards with one acting as the
-                    HDD/FDD controller. Also has a Realtek NIC with both RJ45 and BNC. GPU is a
-                    Cirrus Logic card and is astoundingly shit.
-                </p>
-            </td>
+            <td>Random Whitebox<br>(???)</td>
+            <td>486DX2</td>
+            <td></td>
+            <td>280MB HDD</td>
+            <td>16MB</td>
+            <td>MS-DOS 6.22 &amp; Windows for Workgroups 3.11</td>
         </tr>
         <tr>
-            <td>2023 MacBook Pro 14"</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Apple M3 Pro</li>
-                    <li>18GB RAM</li>
-                    <li>500GB SSD</li>
-                    <li>macOS Sonoma</li>
-                </ul>
-            </td>
+            <td>MacBook Pro 14"<br>(2023)</td>
+            <td colspan="2">M3 Pro</td>
+            <td>500GB SSD</td>
+            <td>18GB</td>
+            <td>macOS Sonoma</td>
         </tr>
         <tr>
-            <td>2018 MacBook Pro 13"</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel i5-8259U @ 2.3GHz</li>
-                    <li>Intel Iris Plus Graphics 655</li>
-                    <li>8GB RAM</li>
-                    <li>250GB SSD</li>
-                    <li>macOS Sonoma</li>
-                </ul>
-                <p class="description">
-                    Old main computer. Really like the touch bar, absolutely hate the butterfly
-                    keyboard.
-                </p>
-            </td>
+            <td>MacBook Pro 13"<br>(2018)</td>
+            <td>Intel i5-8592U (2.3GHz)</td>
+            <td>Intel Iris Plus 655</td>
+            <td>250GB SSD</td>
+            <td>8GB</td>
+            <td>macOS Mojave</td>
         </tr>
         <tr>
-            <td>2012 Lenovo ThinkPad T430</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Core i7</li>
-                    <li>16GB RAM</li>
-                    <li>Windows 7 Professional / NixOS</li>
-                </ul>
-                <p class="description">
-                    One of my main computers. Has been modified to use a classic keyboard instead of
-                    the stock Lenovo keyboard.
-                </p>
-            </td>
+            <td>Lenovo ThinkPad T430<br>(2012)</td>
+            <td>Intel Core i7 (idk what it is)</td>
+            <td></td>
+            <td></td>
+            <td>16GB</td>
+            <td>Windows 7 Pro / NixOS</td>
         </tr>
         <tr>
-            <td>2005 IBM ThinkPad X41T</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Pentium M @ 1.6GHz</li>
-                    <li>Mobile Intel Express Chipset Family (128MB)</li>
-                    <li>1.5GB RAM</li>
-                    <li>40GB HDD</li>
-                    <li>Windows XP Tablet PC Edition</li>
-                </ul>
-            </td>
+            <td>IBM ThinkPad X41T<br>(2005)</td>
+            <td>Intel Pentium M (1.6GHz)</td>
+            <td>Mobile Intel Express Chipset (128MB)</td>
+            <td>40GB HDD</td>
+            <td>1.5GB</td>
+            <td>Windows XP Tablet PC Edition</td>
         </tr>
         <tr>
-            <td>1999 Dell OptiPlex GX1</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Pentium II (Deschutes) @ 400MHz</li>
-                    <li>ATI 3D Rage Pro (4MB)</li>
-                    <li>639MB</li>
-                    <li>40GB HDD</li>
-                    <li>MS-DOS 6.22 &amp; WFW 3.10</li>
-                </ul>
-                <p class="description">
-                    Cool computer that uses Slot 1 CPUs. After a lot of trial and error I managed to
-                    max out the memory. Has a riser that sports 2 PCI and 2 ISA slots (one PCI and
-                    ISA share the same slot).
-                </p>
-            </td>
+            <td>Dell OptiPlex GX1<br>(1999)</td>
+            <td>Intel Pentium II (Deschutes, 400MHz)</td>
+            <td>ATI 3D Rage Pro (4MB)</td>
+            <td>40GB HDD</td>
+            <td>639MB</td>
+            <td>Windows 2000</td>
         </tr>
         <tr>
-            <td>2003 IBM ThinkPad T40</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Pentium M @ 1.3GHz</li>
-                    <li>ATI Mobility Radeon 7500 (32MB)</li>
-                    <li>1GB RAM</li>
-                    <li>30GB HDD</li>
-                    <li>Windows 2000 Professional</li>
-                </ul>
-                <p class="description">
-                    Useful laptop thanks to its parallel port. Has the ubiquitous GPU solder issues
-                    which I &quot;&quot;&quot;&quot;fixed&quot;&quot;&quot;&quot; by jamming a CF card
-                    between the GPU chip and the keyboard.
-                </p>
-            </td>
+            <td>IBM ThinkPad T40<br>(2003)</td>
+            <td>Intel Pentium M (1.3GHz)</td>
+            <td>ATI Mobility Radeon 7500 (32MB)</td>
+            <td>N/A</td>
+            <td>N/A</td>
+            <td>N/A</td>
         </tr>
         <tr>
-            <td>2010 HP Compaq Elite 8100</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Core i7</li>
-                    <li>16GB RAM</li>
-                    <li>some SSD and an HDD</li>
-                    <li>Windows Vista Ultimate (64-bit)</li>
-                </ul>
-            </td>
+            <td>HP Compaq Elite 8100<br>(2010)</td>
+            <td>Intel Core i7 (something or other)</td>
+            <td></td>
+            <td></td>
+            <td>16GB</td>
+            <td>Windows Vista Ultimate (64-bit)</td>
         </tr>
         <tr>
-            <td>2014 Mac mini</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Core i5-4278U @ 2.6GHz</li>
-                    <li>Intel Iris Graphics</li>
-                    <li>8GB RAM</li>
-                    <li>1TB HDD</li>
-                    <li>VMware ESXi 6.7.0u3</li>
-                </ul>
-                <p class="description">
-                    Was used as my VM host for a few years. Has now been superseded by an
-                    actual 1U rack-mount server.
-                </p>
-            </td>
+            <td>Mac mini<br>(2014)</td>
+            <td>Intel Core i5-4278U (2.6GHz)</td>
+            <td>Intel Iris Graphics</td>
+            <td>1TB HDD</td>
+            <td>8GB</td>
+            <td>Proxmox VE 8.2</td>
         </tr>
         <tr>
-            <td>1996 Fujitsu Milan</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Pentium</li>
-                    <li>32MB RAM</li>
-                    <li>1215MB HDD</li>
-                    <li>Windows 98 SE</li>
-                </ul>
-                <p class="description">
-                    Was originally a family members' laptop. Unfortunately the HDD side of the
-                    HDD/FDD cable ripped while I was removing the drive to clean the computer.
-                    Still scouring eBay for a replacement cable (or more likely, an entire
-                    parts machine).
-                </p>
-            </td>
+            <td>Fujitsu Milan<br>(1996)</td>
+            <td>Intel Pentium</td>
+            <td></td>
+            <td>1215MB HDD</td>
+            <td>32MB</td>
+            <td>Windows 98 SE</td>
         </tr>
         <tr>
-            <td>1999 Compaq Armada M300</td>
-            <td>
-                <span class="section-title">Quick Specs</span>
-                <ul>
-                    <li>Intel Pentium III</li>
-                </ul>
-                <p class="description">
-                    Nice little laptop. Mysteriously dead.
-                </p>
-            </td>
+            <td>Compaq Armada M300<br>(1999)</td>
+            <td>Intel Pentium III</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>SuperMicro X9SCM</td>
+            <td>Intel Pentium G850 (2.9GHz)</td>
+            <td>Matrox MGA G6200eW</td>
+            <td>2TB HDD / 80GB HDD</td>
+            <td>16GB</td>
+            <td>Proxmox VE 8.2</td>
+        </tr>
+        <tr>
+            <td>Main PC</td>
+            <td>Intel Core i7-6700K (4GHz)</td>
+            <td>NVidia GTX 1060 (3GB)</td>
+            <td>(multiple)</td>
+            <td>64GB</td>
+            <td>Windows 10 Pro / NixOS</td>
+        </tr>
+        <tr>
+            <td>Toshiba Qosmio F20<br>(2005)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>MacBook Pro 13"<br>(2009)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Packard-Bell EasyNote MIT-LYN01<br>(???)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows XP Home</td>
+        </tr>
+        <tr>
+            <td>Sony VAIO PCG-3B1M<br>(???)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows Vista</td>
+        </tr>
+        <tr>
+            <td>Dell OptiPlex 745 USFF<br>(2006)</td>
+            <td>Intel Pentium Dual Core</td>
+            <td>Intel Integrated</td>
+            <td>(multiple)</td>
+            <td>4GB</td>
+            <td>(multiple)</td>
+        </tr>
+        <tr>
+            <td>Dell Inspiron 1525<br>(2008)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Random Whitebox 2</td>
+            <td>AMD Phenom II X6-1055T</td>
+            <td></td>
+            <td></td>
+            <td>8GB</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Dell Latitude D531<br>(2007)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows XP Professional</td>
+        </tr>
+        <tr>
+            <td>IBM ThinkPad R40<br>(2003)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows 2000</td>
+        </tr>
+        <tr>
+            <td>Dell Latitude CPi<br>(2001)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows 2000</td>
+        </tr>
+        <tr>
+            <td>Dell Latitude CPx<br>(1999)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Windows 98 SE</td>
+        </tr>
+        <tr>
+            <td>Dell Latitude 4898T<br>(???)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Time 8375<br>(???)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Toshiba Satellite 200CDS<br>(1996)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>HP Compaq NC6000<br>(2004)</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>IBM Personal Computer 330<br>(1997)</td>
+            <td>Intel Pentium</td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
+        </tr>
+        <tr>
+            <td>Shuttle XPC SN21G5<br>(2006)</td>
+            <td>AMD Athlon 64 X2</td>
+            <td></td>
+            <td></td>
+            <td>N/A</td>
+            <td>N/A</td>
         </tr>
     </table>
 </x-layout>

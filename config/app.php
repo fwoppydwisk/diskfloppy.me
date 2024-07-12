d52be035d0af95ea6978a8a6019737992a0a8398
Frankie B <git@diskfloppy.me>
2024-07-12 16:24:22 +0100

    Minor fixes

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 return [
     'name' => env('APP_NAME', 'diskfloppy.me'),
-    'version' => '2024.07.11',
+    'version' => '2024.07.12',
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', 'http://localhost'),

fc5cd70e729f639aaf1c6ff6a4bf3d5d52664de9
floppydiskette <git@diskfloppy.me>
2024-08-08 15:41:44 +0100

    Accessibility stuff 2

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,30 +1,30 @@
 <p><strong>Navigation:</strong></p>
 <div class="navlinks">
 <a href="/">
-    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/home2.png') }}" width="16" height="16">Home
+    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/home2.png') }}" width="16" height="16" alt="">Home
 </a><br>
 <a href="//git.diskfloppy.me/">
-    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/repo.png') }}" width="16" height="16">Git
+    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/repo.png') }}" width="16" height="16" alt="">Git
 </a><br>
 <a href="/pub/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/pubfiles.png') }}" width="16" height="16">Public Files
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/pubfiles.png') }}" width="16" height="16" alt="">Public Files
 </a><br>
 <a href="/computers/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/computers.png') }}" width="16" height="16">Computers
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/computers.png') }}" width="16" height="16" alt="">Computers
 </a><br>
 <a href="/calculators/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/calculators.png') }}" width="16" height="16">Calculators
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/calculators.png') }}" width="16" height="16" alt="">Calculators
 </a><br>
 <a href="/bookmarks/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/bookmarks.png') }}" width="16" height="16">Bookmarks
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/bookmarks.png') }}" width="16" height="16" alt="">Bookmarks
 </a><br>
 <a href="/guestbook/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/guestbook.png') }}" width="16" height="16">Guestbook
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/guestbook.png') }}" width="16" height="16" alt="">Guestbook
 </a><br>
 <a href="//weather.diskfloppy.me/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/weather.png') }}" width="16" height="16">Weather
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/weather.png') }}" width="16" height="16" alt="">Weather
 </a><br>
 <a href="/music/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/music.png') }}" width="16" height="16">Music
+	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/music.png') }}" width="16" height="16" alt="">Music
 </a><br>
 </div>

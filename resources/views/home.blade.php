01be7aafc6656cb09222d8fb738685f5cb46ba5c
floppydiskette <floppydisk@hyprcat.net>
2024-08-30 19:50:59 +0100

    Correct steam link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
             <li>Loves ETC desks, prefers Generics to LEDs for some reason</li>
             <li>Has a crippling Soundcraft addiction</li>
             <li>Spends way too much time on his computer</li>
-            <li>Favorite games: <a href="https://steamcommunity.com/id/floppydisk05/recommended/420530/">OneShot</a>, Minecraft, Stardew Valley, N++ and Starbound</li>
+            <li>Favorite games: <a href="https://steamcommunity.com/id/fwoppydwisk/recommended/420530/">OneShot</a>, Minecraft, Stardew Valley, N++ and Starbound</li>
             <li><a href="http://wxqa.com/">CWOP</a> member</li>
         </ul>
         <br>
@@ -22,7 +22,7 @@
             <li><strong>Tech Theatre</strong> - Lighting, Stage Management, etc. (<a href="https://www.controlbooth.com/members/floppydisk.28673/">ControlBooth</a>)</li>
             <li><strong>Programming</strong> - HTML, CSS, JavaScript, C#, Java, PHP, Ruby, Python (<a href="https://github.com/floppydisk05">GitHub</a>)</li>
             <li><strong>Photography</strong> - <a href="https://www.flickr.com/photos/floppydisk/">Flickr</a></li>
-            <li><strong>Gaming</strong> - <a href="https://steamcommunity.com/id/floppydisk05/">Steam Profile</a></li>
+            <li><strong>Gaming</strong> - <a href="https://steamcommunity.com/id/fwoppydwisk/">Steam Profile</a></li>
         </ul>
     </div>
     <div class="section">

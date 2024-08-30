4730f59dd0e37b3b063ed387baeefe3ec6bdf41e
floppydiskette <floppydisk@hyprcat.net>
2024-08-31 00:47:19 +0100

    There will be no bias in my house!

--- resources/views/bookmarks.blade.php
+++ resources/views/bookmarks.blade.php
@@ -3,9 +3,19 @@
     @foreach($categories as $category)
         <div class="section">
             <h2>{{ $category->name }}</h2>
+            @if($category->id == 1)
+                <p><em>(These are shuffled every load)</em></p>
+                @php
+                    $sites = $category->sites->shuffle();
+                @endphp
+            @else
+                @php
+                    $sites = $category->sites;
+                @endphp
+            @endif
             <hr>
             <ul>
-            @foreach($category->sites as $site)
+            @foreach($sites as $site)
                     <li><a href="{{ $site->url }}">{{ $site->name }}</a> - {{ $site->description }}</li>
             @endforeach
             </ul>

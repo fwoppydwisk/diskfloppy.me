098d68b393fdeee232f3ce539de5731431f6cd66
floppydiskette <floppydisk@hyprcat.net>
2024-11-04 23:12:59 +0000

    woah is that a new design

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,30 +1,14 @@
-<p><strong>Navigation:</strong></p>
-<div class="navlinks">
-<a href="/">
-    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/home2.png') }}" width="16" height="16" alt="">Home
-</a><br>
-<a href="//git.diskfloppy.me/">
-    <img class="pixel navbar-icon" src="{{ asset('images/icons/nav/repo.png') }}" width="16" height="16" alt="">Git
-</a><br>
-<a href="/pub/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/pubfiles.png') }}" width="16" height="16" alt="">Public Files
-</a><br>
-<a href="/computers/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/computers.png') }}" width="16" height="16" alt="">Computers
-</a><br>
-<a href="/calculators/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/calculators.png') }}" width="16" height="16" alt="">Calculators
-</a><br>
-<a href="/bookmarks/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/bookmarks.png') }}" width="16" height="16" alt="">Bookmarks
-</a><br>
-<a href="/guestbook/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/guestbook.png') }}" width="16" height="16" alt="">Guestbook
-</a><br>
-<a href="//weather.diskfloppy.me/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/weather.png') }}" width="16" height="16" alt="">Weather
-</a><br>
-<a href="/music/">
-	<img class="pixel navbar-icon" src="{{ asset('images/icons/nav/music.png') }}" width="16" height="16" alt="">Music
-</a><br>
-</div>
+<nav>
+    <p><strong>Navigation:</strong></p>
+    <div class="navlinks">
+        <a href="/">/home/niko</a><br>
+        <a href="//git.diskfloppy.me/">~/git</a><br>
+        <a href="/pub/">~/pub</a><br>
+        <a href="/computers/">~/computers</a><br>
+        <a href="/calculators/">~/calculators</a><br>
+        <a href="/bookmarks/">~/bookmarks</a><br>
+        <a href="/guestbook/">~/guestbook</a><br>
+        <a href="//weather.diskfloppy.me/">~/weather</a><br>
+        <a href="/music/">~/music</a><br>
+    </div>
+</nav>

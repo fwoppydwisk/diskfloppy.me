3ee7cc2448ac61b2389f69ac741f407348aa39b6
floppydiskette <floppydisk@hyprcat.net>
2024-12-07 01:10:40 +0000

    Nuke unused pages

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\BookmarksController;
-use App\Http\Controllers\CalculatorsController;
-use App\Http\Controllers\ComputersController;
 use App\Http\Controllers\GuestbookController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MusicController;
@@ -20,12 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Run the PageView middleware for *all* public GET routes
 Route::get('/', [HomeController::class, 'show']);
 Route::get('/bookmarks', [BookmarksController::class, 'show']);
 Route::get('/guestbook', [GuestbookController::class, 'show']);
-Route::get('/calculators', [CalculatorsController::class, 'show']);
-Route::get('/computers', [ComputersController::class, 'show']);
 Route::get('/music', [MusicController::class, 'show']);
 Route::get('/privacy', [PrivacyController::class, 'show']);
 Route::post('/guestbook', [GuestbookController::class, 'addEntry'])

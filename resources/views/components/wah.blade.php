5596afa450ef2c4c98d033f5579955c8a4484157
floppydiskette <floppydisk@hyprcat.net>
2024-12-09 20:48:27 +0000

    Handle no image being returned by tinyfox.dev

--- resources/views/components/wah.blade.php
+++ resources/views/components/wah.blade.php
@@ -1,5 +1,10 @@
 <div class="wah">
     <h3>Random Wah!</h3>
-    <img src="{{ $wah }}">
-    <p>Image "stolen" from <a href="https://tinyfox.dev/">tinyfox.dev</a></p>
+    @if ($wah !== "")
+        <img src="{{ $wah }}" alt="Random image featuring a Red Panda">
+        <p>Image "stolen" from <a href="https://tinyfox.dev/">tinyfox.dev</a></p>
+    @else
+        <img src="https://placehold.co/250x250" alt="250x250 Placeholder Image">
+        <p>Unable to retrieve image</p>
+    @endif
 </div>

70ac42f2f5c155a664795b722db81c5fd63896ca
floppydiskette <floppydisk@hyprcat.net>
2024-12-21 00:21:18 +0000

    Show album cover for current track

--- resources/views/components/current-track.blade.php
+++ resources/views/components/current-track.blade.php
@@ -1,4 +1,9 @@
 <div class="current-track">
-    <h2>Last/Current Track:</h2>
-    <a href="{{ $track["url"] }}">{{ $track["title"] }} • {{ $track["artist"] }}</a><br>
+    <div>
+        <img src="{{ $track["image"] }}" alt="Album cover for {{ $track["title"] }} by {{ $track["artist"] }}">
+    </div>
+    <div>
+        <h2>Last/Current Track:</h2>
+        <a href="{{ $track["url"] }}">{{ $track["title"] }} • {{ $track["artist"] }}</a><br>
+    </div>
 </div>
